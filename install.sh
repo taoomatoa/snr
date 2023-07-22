785730571e50de283d8e2979b056366685488c40
Taoo Matoa <131985316+taoomatoa@users.noreply.github.com>
2023-07-22 16:27:48 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -251,7 +251,7 @@ wget https://raw.githubusercontent.com/taoomatoa/src/ipuk/xray/ins-xray.sh && ch
 #wget https://raw.githubusercontent.com/taoomatoa/src/ipuk/bckp/set-br.sh && chmod +x set-br.sh && ./set-br.sh
 wget https://raw.githubusercontent.com/taoomatoa/src/ipuk/sshws/insshws.sh && chmod +x insshws.sh && ./insshws.sh
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "$green          FRANATA STORE              $NC"
+echo -e "$green          TAOO MATOA              $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 sleep 2
 clear
