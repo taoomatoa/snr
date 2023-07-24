ebaf2af0fd7a979a3da13d21bb7767c5d35e3787
Taoo Matoa <131985316+taoomatoa@users.noreply.github.com>
2023-07-25 01:51:16 +0700

    Update cekws.sh

--- xray/cekws.sh
+++ xray/cekws.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
 
 clear
 echo -n > /tmp/other.txt
