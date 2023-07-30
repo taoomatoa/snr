2b98fc76fbfa6442596a140d7c6b4f05623b2d4f
Taoo Matoa <131985316+taoomatoa@users.noreply.github.com>
2023-07-30 23:29:34 +0700

    Update trial-menu.sh

--- menu_all/trial-menu.sh
+++ menu_all/trial-menu.sh
@@ -71,12 +71,6 @@ echo -e "\033[1;37mNB: Trial account will not logged into create log user\033[0m
 \033[1;32mTrial-Generator\033[0m :
 
 [\033[0;32m01\033[0m] • Generate Trial SSH 
-[\033[0;32m02\033[0m] • Generate Trial Wireguard
-[\033[0;32m03\033[0m] • Generate Trial L2TP
-[\033[0;32m04\033[0m] • Generate Trial PPTP
-[\033[0;32m05\033[0m] • Generate Trial SSTP
-[\033[0;32m06\033[0m] • Generate Trial Shadowsocks-R
-[\033[0;32m07\033[0m] • Generate Trial Shadowsocks
 [\033[0;32m08\033[0m] • Generate Trial Vmess
 [\033[0;32m09\033[0m] • Generate Trial VLESS
 [\033[0;32m10\033[0m] • Generate Trial Trojan WS/GRPC
@@ -94,43 +88,15 @@ case "$x" in
    ;;
    2 | 02)
    clear
-   trial-wg
+   trial-ws
    ;;
    3 | 03)
    clear
-   trial-l2tp
+   trial-vless
    ;;
    4 | 04)
    clear
-   trial-pptp
-   ;;
-   5 | 05)
-   clear
-   trial-sstp
-   ;;
-   6 | 06)
-   clear
-   trial-ssr
-   ;;
-   7 | 07)
-   clear
-   trial-ss
-   ;;
-   8 | 08)
-   clear
-   trial-ws
-   ;;
-   9 | 09)
-   clear
-   trial-vless
-   ;;
-   10)
-   clear
    trial-tr
-   ;;
-   11)
-   clear
-   trial-trgo
    ;;
    0 | 00)
    clear
