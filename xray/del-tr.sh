053adc52d5cbc81963f69fe29124e9c1c9fa012c
Taoo Matoa <131985316+taoomatoa@users.noreply.github.com>
2023-07-31 10:43:23 +0700

    Update del-tr.sh

--- xray/del-tr.sh
+++ xray/del-tr.sh
@@ -75,7 +75,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#! " "/etc/xray/config.json")
 		echo ""
 		echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 		read -n 1 -s -r -p "Press any key to back on menu"
-        v2ray-menu
+        trojan-menu
 	fi
 
 	clear
